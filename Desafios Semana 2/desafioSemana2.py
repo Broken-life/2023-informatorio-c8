819739fcd679c2d0a5840e9e6d2983d68034363b
Broken-life <yamfogar@gmail.com>
2023-05-05 20:53:08 -0300

    desafio 2 terminado

--- Desafios Semana 2/desafioSemana2.py
+++ Desafios Semana 2/desafioSemana2.py
@@ -1,40 +1,40 @@
-frase = str(input("Ingrese una frase, palabra, o texto:").lower()) #pasamos todo a minuscula
+### ---Desafio 2 --- ### 
+frase = str(input("Ingrese una frase, palabra, o texto:").lower()) 
 
-#eliminar los signos de puntuacion de la frase con un map, para dejar solo letra
-# signos = [",", ".", ";", ":", "¿", "?", "¡", "!", "(", ")", '"', "'"]
-# frase2 = list(map(lambda x: x if x not in signos else " ", frase))
-# frase2 = "".join(frase2)
-# print(frase)
+#reemplazamos los caracteres que no sean letras, numeros o espacios por un espacio vacio
+for caracter in frase:
+    if caracter.isalpha() or caracter.isdigit() or caracter.isspace():
+        continue
+    else:
+        frase = frase.replace(caracter, '')
 
-letra_1 = str(input("Ingrese una letra: "))
-letra_2 = str(input("Ingrese otra letra: "))
-letra_3 = str(input("Ingrese otra letra: "))
-
-#metemos las letras en una lista
-letras = [letra_1.lower(), letra_2.lower(), letra_3.lower()]
+letras_elegidas = []
+for letra in range(3):
+    letras_elegidas.append(str(input(f"Ingrese una letra {letra+1}: ").lower()))
 
 #cantidad de veces que aparece cada letra elegida en el texto ingresado
-for letra in letras:
-    frase.count(letra) 
-    print(f"La letra {letra} aparece {frase.count(letra)} veces en la frase")
+for letra in letras_elegidas:
+    cant_letras = frase.count(letra) 
+    print(f"La letra {letra} aparece {cant_letras} veces")
 
 #cantidad de palabras que tiene la frase ingresada
 frase.split()
-print(f"La frase tiene {len(frase.split())} palabra/s")
+longitud_frase = len(frase.split())
+print(f"La frase ingresada tiene {longitud_frase} palabras")
 
 #primera letra y ultima letra de la frase ingresada
-print(f"La primera letra de la frase es {frase[0]} y la ultima letra es {frase[-1]}")
+primera_letra = frase[0]
+ultima_letra = frase[-1]
+print(f"La primera letra de la frase es {primera_letra} y la última letra es {ultima_letra}")
 
 #mostrar el texto ingresado, en orden inverso
-print(f"El texto ingresado en orden inverso es {frase[::-1]}")
+frase_invertida = frase[::-1]
+print(f"El texto ingresado en orden inverso es: {frase_invertida}")
 
-#verificar si la palabra "Python" se encuentra en el texto ingresado
-if "python" in frase:
-    print("La palabra python se encuentra en el texto ingresado")
+#Usando un diccionario para determinar si "python" se encuentra en el texto ingresado":
+python_in_text = "python" in frase
+options ={"si": True , "no": False} 
+if python_in_text == options["si"]:
+    print("La palabra 'Python' se encuentra en el texto ingresado :) ")
 else:
-    print("La palabra python no se encuentra en el texto ingresado")
-
-## con un diccionario
-# options = {True: "se encuentra en el texto", False: "no se encuentra en el texto"}
-# python_in_text = "python" in frase
-# print(f"La palabra python {options[python_in_text]}")
+    print("La palabra 'Python' no se encuentra en el texto ingresado :( ")
